b7d54c76029a4f15b9a0fb1a5e1ce9ef77e0d41d
maelysrll <maelys.roulleau@hotmail.com>
2023-02-06 09:26:32 +0100

    Liste des astronautes dans l’espace

---
+++ astronautes.py
@@ -0,0 +1,9 @@
+import requests
+from pprint import pprint
+
+astro= requests.get("http://api.open-notify.org/astros.json").json()
+
+# afficher la liste des astronautes actuellement dans l'espace
+for personne in astro["people"]:
+    print(personne)
+
